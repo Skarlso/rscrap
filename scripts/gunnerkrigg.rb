b60a8dfc661612858310ca6a4f7640f6678531e3
Gergely Brautigam <skarlso777@gmail.com>
2016-08-30 14:55:20 +0200

    Added nokogiri parsing of gunnerkrigg court.

--- scripts/gunnerkrigg.rb
+++ scripts/gunnerkrigg.rb
@@ -1,4 +1,12 @@
-puts 'Scrap gunnerkrigg web comic.'
 require_relative '../helper'
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'sqlite3'
 
-send_message('Test')
+url = 'http://www.gunnerkrigg.com'
+page = Nokogiri::HTML(open(url))
+comic_id = page.css('img.comic_image')[0].select { |e| e if e[0] == 'src' }[0][1]
+
+new_comic = "#{url}#{comic_id}"
+send_message("Got you a new Gunnerkrigg Court comic: #{new_comic}")
