46dd99aacd0000c9b58185c026549ab645b98c38
Gergely Brautigam <skarlso777@gmail.com>
2016-08-31 08:02:56 +0200

    Added goblinscomic parser.

--- scripts/goblinscomic.rb
+++ scripts/goblinscomic.rb
@@ -1,1 +1,14 @@
-puts 'This will scrap goblins comics web page.'
+require_relative '../helper'
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+help = Helper.new
+url = 'http://www.goblinscomic.org/'
+page = Nokogiri::HTML(open(url))
+new_comic = page.css('div#comic img')[0].select { |e| e if e[0] == 'src' }[0][1]
+old_comic = help.get_old_comic_id(url)
+
+
+help.save_comic_id(url, old_comic, new_comic)
+help.send_message("Got you a new Goblins comic: #{new_comic}")
