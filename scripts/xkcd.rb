06750850ea15cd882ac3f91c23464dcd2096c422
Gergely Brautigam <skarlso777@gmail.com>
2016-09-02 20:48:19 +0200

    Added xkcd.

---
+++ scripts/xkcd.rb
@@ -0,0 +1,10 @@
+require_relative '../helper'
+require 'nokogiri'
+require 'open-uri'
+
+url = 'http://xkcd.com/'
+help = Helper.new
+page = Nokogiri::HTML(open(url))
+comic_id = page.css('div#comic').css('img')[0].select { |e| e if e[0] == 'src' }[0][1]
+new_comic = "http:#{comic_id}"
+help.send_new_comic(url, new_comic)
