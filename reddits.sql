cbc599eec8cb1f5451297e62b465d892dedda258
Gergely Brautigam <skarlso777@gmail.com>
2016-09-20 15:08:35 +0200

    Added in the SQL files for both tables.

---
+++ reddits.sql
@@ -0,0 +1,6 @@
+create table reddits (
+       subreddit varchar(100),
+       postid varchar(100),
+       stamp integer,
+       PRIMARY KEY (postid)
+);
---
+++ websites.sql
@@ -0,0 +1,5 @@
+create table websites (
+       url varchar(100),
+       id varchar(100),
+       PRIMARY KEY (url)
+);
